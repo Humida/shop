0185cfc4c3383eb1a469a8ae1a08025b2a16305e
Humida <huynhminhdanh1110@gmail.com>
2021-04-25 23:00:47 +0700

    create cart

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
-const homeControllor = require('./src/app/controller/home.controller');
+const homeController = require('./src/app/controller/home.controller');
 
 // require route
 const productRouter = require('./src/router/product.router')
@@ -34,11 +34,15 @@ db.connect();
 
 app.use('/shirt', productRouter);
 
-app.get('/', homeControllor);
+app.get('/', homeController);
+
+app.get('/demo', function(req, res, next) {
+    res.render('cart');
+})
 
 const port = process.env.PORT || 4000;
 
 console.log(process.env.MONGODB);
 app.listen(port, () => {
-    console.log(`app is lintening on port :${port}`);
+    console.log(`app is listening on port :${port}`);
 })
